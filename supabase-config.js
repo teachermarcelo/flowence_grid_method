644921b19fc6154c1f98610862d1f0fa8d9e8446
Fluent Immersion <158618477+teachermarcelo@users.noreply.github.com>
2026-05-12 23:09:02 -0300

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,6 +0,0 @@
-// supabase-config.js
-// Substitua pelos valores do seu projeto: Supabase > Settings > API
-window.SUPABASE_CONFIG = {
-  url: 'https://rvgcniaowzmsudzliozf.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2Z2NuaWFvd3ptc3Vkemxpb3pmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MjQxNzQsImV4cCI6MjA5MTQwMDE3NH0.uwwKFLuK-XyPoXPrB6_CseRTiD9d-iyMQSPWrFw-l-I'
-};
