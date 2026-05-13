6b7330c146e813d383f34a22d421403ca35a9e5e
Fluent Immersion <158618477+teachermarcelo@users.noreply.github.com>
2026-05-12 23:12:43 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,3 @@
-// supabase-config.js
-// Credenciais do projeto Supabase
 window.SUPABASE_CONFIG = {
   url: 'https://rvgcniaowzmsudzliozf.supabase.co',
   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2Z2NuaWFvd3ptc3Vkemxpb3pmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MjQxNzQsImV4cCI6MjA5MTQwMDE3NH0.uwwKFLuK-XyPoXPrB6_CseRTiD9d-iyMQSPWrFw-l-I'
